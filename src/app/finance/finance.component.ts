35cc527f4af28771baa2e5d364d949e459b43e89
gnz16 <ganeriwala.home@gmail.com>
2020-08-24 00:39:44 +0530

    initial commit

---
+++ src/app/finance/finance.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-finance',
+  templateUrl: './finance.component.html',
+  styleUrls: ['./finance.component.css']
+})
+export class FinanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView();
+}
+
+}
